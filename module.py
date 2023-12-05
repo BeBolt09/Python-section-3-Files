d1deb3b0281285e1a46c287d43bd2bd768163957
Sebastien Dupont <sebastiengdupont@gmail.com>
2023-12-05 13:07:36 -0800

    finished exercises
    Update of all exercises and modules

---
+++ module.py
@@ -0,0 +1,9 @@
+def printName(name):
+    return f"Hello Mr/Ms {name}...we've been waiting for you!"
+
+
+def surfaceArea(x,y):
+    return x*y
+
+def circleCircom(radius):
+    return 2*(3.14)*radius
